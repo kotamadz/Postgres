74bed9f8e07b56e7c3cfe7a2b01d5351e4316f1b
kotamadz <34414166+kotamadz@users.noreply.github.com>
2020-03-03 23:32:53 +0100

    Update pg_get_easter_ascension_pentecost.sql

--- pl_pg/pg_get_functions/pg_get_easter_ascension_pentecost.sql
+++ pl_pg/pg_get_functions/pg_get_easter_ascension_pentecost.sql
@@ -1,5 +1,10 @@
 -- funtion public.pg_get_easter_ascension_pentecost
--- drop function
+-- returns record contains 3 fields :
+-- easter day
+-- ascension day
+-- pentecost day
+-- year between 1900 - 2099 ( 200 years )  type integer
+-- drop function if exists
 drop function if exists public.pg_get_easter_ascension_pentecost cascade;
 create or replace function public.pg_get_easter_ascension_pentecost(
 	yyyy integer default null::integer,
@@ -17,19 +22,41 @@ as $body$
 	   easter     := null;
 	   ascension  := null;
 	   pentencost := null;
-    else
-           easter     := (select pg_get_easter_day(yyyy));
+	else
+       easter     := (select pg_get_easter_day(yyyy));
 	   ascension  := easter +  interval '39 day';
 	   pentencost := easter +  interval '49 day';
-    end if;
+	end if;
   end;
+					       
 $body$;
 
+-- Exemple to use :					       			       
 /*
+					       
 select 
    yyyy, 
   (select easter     from public.pg_get_easter_ascension_pentecost(yyyy)) as easter,
   (select ascension  from public.pg_get_easter_ascension_pentecost(yyyy)) as ascension,
   (select pentencost from public.pg_get_easter_ascension_pentecost(yyyy)) as pentencost
 from years;
+
+create table years(yyyy integer);
+insert into years (yyyy) values (2010);
+insert into years (yyyy) values (2011);
+insert into years (yyyy) values (2012);
+insert into years (yyyy) values (2013);
+insert into years (yyyy) values (2014);
+insert into years (yyyy) values (2015);
+insert into years (yyyy) values (2016);
+insert into years (yyyy) values (2017);
+insert into years (yyyy) values (2018);
+insert into years (yyyy) values (2019);
+insert into years (yyyy) values (2020);
+insert into years (yyyy) values (2021);
+insert into years (yyyy) values (2022);
+insert into years (yyyy) values (2023);
+insert into years (yyyy) values (2024);
+insert into years (yyyy) values (2025);
+					       
 */
