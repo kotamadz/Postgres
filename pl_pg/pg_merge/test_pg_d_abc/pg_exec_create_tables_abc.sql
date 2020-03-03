28679c5aa420e4c0653c86ee50a0219653ee084c
kotamadz <34414166+kotamadz@users.noreply.github.com>
2020-03-04 00:38:03 +0100

    Update pg_exec_create_tables_abc.sql

--- pl_pg/pg_merge/test_pg_d_abc/pg_exec_create_tables_abc.sql
+++ pl_pg/pg_merge/test_pg_d_abc/pg_exec_create_tables_abc.sql
@@ -1,11 +1,12 @@
+-- Programmed by Kotama.dz
 /*
 function pg_exec_create_tables(
         in  src_schema character varying default null::character varying,
-		in  src_table  character varying default null::character varying,
+	in  src_table  character varying default null::character varying,
         in  his_table  character varying default null::character varying,
-		in  trg_schema character varying default null::character varying,
-		in  trg_table  character varying default null::character varying, 
-		in  is_drop    boolean default false)
+	in  trg_schema character varying default null::character varying,
+	in  trg_table  character varying default null::character varying, 
+	in  is_drop    boolean default false)
 */
 
 -- execute create tables with 5 arguments are initialize to null by default but is_drop to false by default
