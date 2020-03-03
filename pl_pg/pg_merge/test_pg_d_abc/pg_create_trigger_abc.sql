ff843b2bd15a79d77c0d9a4ba8f78c67f54259c5
kotamadz <34414166+kotamadz@users.noreply.github.com>
2020-03-04 00:29:29 +0100

    Add files via upload

---
+++ pl_pg/pg_merge/test_pg_d_abc/pg_create_trigger_abc.sql
@@ -0,0 +1,9 @@
+-- create trigger on table d_abc
+-- pg_create_trigger_abc.sql
+
+drop trigger if exists pg_exec_merge_trigger_abc on d_abc;
+create trigger pg_exec_merge_trigger_abc
+  after insert or update or delete
+  on d_abc
+  execute procedure pg_exec_merge_trigger('public', 'd_abc', 'd_abc_hist', 'datamart', 'dim_abc', 'id');
+  
---
+++ pl_pg/pg_merge/test_pg_d_abc/pg_exec_merge_abc.sql
@@ -0,0 +1,16 @@
+/*
+    function pg_exec_merge(
+        in  src_schema   character varying default null::character varying,
+		in  src_table    character varying default null::character varying,
+        in  his_table    character varying default null::character varying,
+		in  trg_schema   character varying default null::character varying,
+		in  trg_table    character varying default null::character varying,
+		in  bk           character varying default null::character varying,
+		out deleted_row  integer,
+		out inserted_row integer,
+		out updated_row  integer,
+	    out pg_exec_state   text,
+		out pg_exec_msg     text)
+*/
+
+select * from pg_exec_merge('public', 'd_abc', 'd_abc_hist', 'datamart', 'dim_abc', 'id');
