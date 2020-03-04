d724eb78f5b545a4bd02610a2a5a0d3e9e723760
kotamadz <34414166+kotamadz@users.noreply.github.com>
2020-03-04 10:02:32 +0100

    Update pg_get_date_full_en_fr_nl.sql

--- pl_pg/pg_with/py_with_functions/pg_get_date_full_en_fr_nl.sql
+++ pl_pg/pg_with/py_with_functions/pg_get_date_full_en_fr_nl.sql
@@ -1,3 +1,11 @@
+-- Programmed by Kotama.dz(ETL Developper/ BI Cognos)
+-- in tsmp :: type timestamp or date
+-- returns record with 3 fields :
+-- date_full_en :: full date in English
+-- date_full_fr :: full date in French
+-- date_full_nl :: full date in Dutch
+
+drop function if exists public.pg_get_date_full_en_fr_nl cascade;
 create or replace function public.pg_get_date_full_en_fr_nl(
 	tmsp timestamp without time zone default null::timestamp without time zone,
 	out date_full_en text,
@@ -19,21 +27,21 @@ declare
 	   date_full_en := null;
 	   date_full_fr := null;
 	   date_full_nl := null;
-	else
-	    fl := (select date_long_fr from pg_get_date_long_en_fr_nl(tmsp));
-		df := (select day_name_fr  from pg_get_day_name_en_fr_nl(tmsp));
+    else
+	   fl := (select date_full_fr from pg_get_date_full_en_fr_nl(tmsp));
+	   df := (select day_name_fr  from pg_get_day_name_en_fr_nl(tmsp));
 		
-		nl := (select date_long_nl from pg_get_date_long_en_fr_nl(tmsp));
-		dn := (select day_name_nl  from pg_get_day_name_en_fr_nl(tmsp));
+	   nl := (select date_full_nl from pg_get_date_full_en_fr_nl(tmsp));
+	   dn := (select day_name_nl  from pg_get_day_name_en_fr_nl(tmsp));
 		
-		el := (select date_long_en from pg_get_date_long_en_fr_nl(tmsp));
-		de := (select day_name_en  from pg_get_day_name_en_fr_nl(tmsp));
+	   el := (select date_full_en from pg_get_date_full_en_fr_nl(tmsp));
+	   de := (select day_name_en  from pg_get_day_name_en_fr_nl(tmsp));
 		
-	    date_full_en := de || ' ' || el;
-	    date_full_fr := df || ' ' || fl;
-	    date_full_nl := dn || ' ' || nl;  
+	   date_full_en := de || ' ' || el;
+	   date_full_fr := df || ' ' || fl;
+	   date_full_nl := dn || ' ' || nl;  
 		
-	end if;
+    end if;
   end;
 $BODY$;
 
