e17a064815a0fdfed00a69bc21bd7661f5c5b5f5
kotamadz <34414166+kotamadz@users.noreply.github.com>
2020-03-04 10:25:37 +0100

    Update py_exec_create_tables_abc.sql

--- pl_py/py_merge/test_py_d_abc/py_exec_create_tables_abc.sql
+++ pl_py/py_merge/test_py_d_abc/py_exec_create_tables_abc.sql
@@ -1,18 +1,19 @@
+-- Programmed by Kotama.dz
 /*
 function py_exec_create_tables(
         in  src_schema character varying default null::character varying,
-		in  src_table  character varying default null::character varying,
+	in  src_table  character varying default null::character varying,
         in  his_table  character varying default null::character varying,
-		in  trg_schema character varying default null::character varying,
-		in  trg_table  character varying default null::character varying, 
-		in  is_drop    boolean default false)
+	in  trg_schema character varying default null::character varying,
+	in  trg_table  character varying default null::character varying, 
+	in  is_drop    boolean default false)
 */
 
--- execute create tables with null 5 arguments is initialize to false by default
--- in this case the function drop not existing tables
+-- execute create tables with 5 arguments are initialize to null by default but is_drop to false by default
+-- in this case the function don't drop existing tables
 select * from py_exec_create_tables('public', 'd_abc', 'd_abc_hist', 'datamart', 'dim_abc');
 or 
--- in this case the function drop not existing tables
+-- in this case the function don't drop existing tables
 select * from py_exec_create_tables('public', 'd_abc', 'd_abc_hist', 'datamart', 'dim_abc', false);
 or
 -- in this case the function drop existing tables
